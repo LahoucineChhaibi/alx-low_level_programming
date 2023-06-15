6fb6f9ca27154b3ce69fb79182a317b9e4111490
Lahoucine <lahoucine.chhaibi@gmail.com>
2023-06-15 07:13:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
-include <stdio.h>
+#include <stdio.h>
+/**
+ *  main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
