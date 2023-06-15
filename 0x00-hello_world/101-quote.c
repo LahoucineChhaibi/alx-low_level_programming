5774fcbf195f461e93265766ec36c99e826e0d72
Lahoucine <lahoucine.chhaibi@gmail.com>
2023-06-15 07:46:22 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a program Prints out line to the standard error.
+ * Return: 1 ( success).
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
