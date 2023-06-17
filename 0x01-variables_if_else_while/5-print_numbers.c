6863b47e1d09850cbed062c44c8361cacf2494af
Lahoucine <lahoucine.chhaibi@gmail.com>
2023-06-17 15:48:18 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit Testbers of base 10 starting from 0.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	int Test;
+
+	for (Test = 0; Test < 10; Test++)
+		printf("%d", Test);
+
+	printf("\n");
+
+	return (0);
+}
