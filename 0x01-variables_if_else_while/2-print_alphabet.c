1c4354eaf6ac216ae4938dc2d568748990cd8e2f
Lahoucine <lahoucine.chhaibi@gmail.com>
2023-06-17 15:20:59 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,33 +1,17 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Print num from 0 to 99.
- * Num must be separated by commas followed by a space.
- * You can only use `putchar` to print to console.
- * You can only use `putchar` up to 5 times.
- * Return: 0
+ * main - Prints the alphabet in lowercase.
+ * Return: always 0.
  */
 int main(void)
 {
-	int a;
-	int b;
-	int c;
+	char Test;
 
-	for (a = 0; a < 100; a++)
-	{
-		b = a / 10;
-		c = a % 10;
+	for (Test	= 'a'; Test <= 'z'; Test++)
+		putchar(Test);
 
-		putchar(b + '0');
-		putchar(c + '0');
-
-		if (a < 99)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
 	putchar('\n');
 
-	return (a);
+	return (0);
 }
