0ea50b0e921c880cad3ebf96baee6d1e9f9568f3
Lahoucine <lahoucine.chhaibi@gmail.com>
2023-06-17 16:13:03 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,7 +4,7 @@
  * main - Prints all possible combinations of two different digits,
  *        in ascending order, separated by a comma followed by a space.
  *
- * Return: always 0.
+ * Return: always 0 (Success)
  */
 int main(void)
 {
