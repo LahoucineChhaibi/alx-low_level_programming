89e48c596edc23690bfb0e1979bed49020650ce5
Lahoucine <lahoucine.chhaibi@gmail.com>
2023-06-29 14:03:37 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,8 +10,8 @@ char *rot13(char *s)
 {
 	int i, j;
 
-	char a[] = "QASXWEDCVERFGBTGHNMUYJMIKOLPwqasxedfvrtgbyhjnmikolp";
-	char b[] = "ASDFGHJKLQWERTYUIOPZXCVBNMzxcvbnmasdfghjkqwertyuiop";
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
